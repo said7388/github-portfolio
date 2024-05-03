4925452933d7bd1e87f5e8dc5c7ddf8bcceb8e8b
said7388 <mdabusaidriyaz@gmail.com>
2024-05-03 15:23:40 +0600

    update readme

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="bg-transparent">
+    <nav className="bg-transparent hidden md:block">
       <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center opacity-0 md:opacity-100">
           <Link
